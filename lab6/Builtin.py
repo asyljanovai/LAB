224fad704f7d8f4fb29968f537c894abac8d4f63
asyljanovai <157474502+asyljanovai@users.noreply.github.com>
2024-03-07 09:35:29 +0500

    Add files via upload

--- lab6/Builtin.py
+++ lab6/Builtin.py
@@ -1,6 +1,45 @@
 #1
-l = [0, 24, 2, 5, 7]
+""" l = [0, 24, 2, 5, 7]
 m = 2
-for x in iter(l):
-    x *= 2
-print(l)
+l = list(map(lambda a:a*m, l))
+print(l) """
+
+#2
+""" def upperlower(s):
+    if s.isupper():
+        return 1
+    if s.islower():
+        return 0
+
+string = str(input()).replace(" ", "")
+l = list(string)
+l = list(map(upperlower, l))
+l = sum(l)
+uppercase = l
+lowercase = len(string) - l
+print("Uppercase:",uppercase)
+print("Lowercase:",lowercase) """
+
+#3
+""" string = str(input())
+stringr = "".join(reversed(string))
+print(stringr)
+if string == stringr:
+    print("A palindrome")
+else:
+    print("Not a palindrome") """
+
+
+#4
+""" import time
+import math
+val = int(input())
+delay = int(input())
+time.sleep(delay*0.001)
+print("Square root of", val, "after", delay, "milliseconds is", math.sqrt(val)) """
+
+#5
+sampletuple1 = (True, False, True)
+sampletuple2 = (True, True, True, True, True)
+print(all(sampletuple1))
+print(all(sampletuple2))
